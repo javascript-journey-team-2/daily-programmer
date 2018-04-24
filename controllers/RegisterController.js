e1f18fc185d7fa8d3907af657e279f145765513c
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-04-24 13:57:39 +0700

    buat registrasi langsung login

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,13 +1,14 @@
 var models  = require('../models/');
 var crypto = require('crypto');
 var secret = 'andaglos'; 
+var session_store;
 
 exports.index = function(req, res) {
 	res.render('register/index');
 };
 
 exports.store = function(req, res) {
-
+	session_store = req.session;
 	var passwordEncripsi = crypto.createHmac('sha256', secret)
 	.update(req.param('password'))
 	.digest('hex');
@@ -29,39 +30,38 @@ exports.store = function(req, res) {
         v_last_name = req.sanitize( 'last_name' ).escape().trim();
 
         models.Users.findOne({ where: { username: req.param('username') } }).then(regist => { 
-        	console.log(regist);
-            if (regist == null) { 
-               				
-               				models.Users.create({ 
-							    username: req.param('username'),
-							    first_name: req.param('first_name'),
-							    last_name: req.param('last_name'),
-							    password: passwordEncripsi,
-							    email: req.param('email'),
-							    alamat: req.param('alamat'),
-							    jabatan: req.param('jabatan'),
-							  }).then(function (err) {
-								  	if (!err){
-				                        console.log(err);
-				                        req.flash('msg_error', 'Maaf, sepertinya ada masalah dengan sistem kami...');
-				                        res.redirect('/register');
-				                    }
-				                    else{
-					                    req.flash('msg_info', 'Registrasi berhasil...');
-					                    res.redirect('/register');
-					                }
-						    });
+            if (regist == null) { 				
+               		models.Users.create({ 
+						username: req.param('username'),
+						first_name: req.param('first_name'),
+						last_name: req.param('last_name'),
+						password: passwordEncripsi,
+						email: req.param('email'),
+						alamat: req.param('alamat'),
+						jabatan: req.param('jabatan'),
+					}).then(function (registerr) {
+						console.log(registerr);	
+						session_store.id = registerr.dataValues.id;
+						session_store.username = registerr.dataValues.username;
+						session_store.email = registerr.dataValues.email;
+						session_store.first_name = registerr.dataValues.first_name;
+						session_store.last_name = registerr.dataValues.last_name;
+						session_store.jabatan = registerr.dataValues.jabatan;
+						session_store.logged_in = true;
+
+						console.log(session_store);
+						res.redirect('/home');       
+					});
             }
             else{
-                req.flash('msg_error', 'Maaf, username sudah digunakan...');
-                res.render('register', {
-		            username: req.param('username'),
-		            email: req.param('email'),
-		            first_name: req.param('first_name'),
-		            last_name: req.param('last_name'),
-		        });
+	                req.flash('msg_error', 'Maaf, username sudah digunakan...');
+	                res.render('register', {
+			            username: req.param('username'),
+			            email: req.param('email'),
+			            first_name: req.param('first_name'),
+			            last_name: req.param('last_name'),
+			        });
             }
-
         });
     }
     else
