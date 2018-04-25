cf1f711a4e7f138b5b4647192b83e330a70b171c
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-04-25 14:58:51 +0700

    PErbaikan Validasi proses registrasi

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -14,31 +14,53 @@ exports.store = function(req, res) {
 	.digest('hex');
 
 	// validasi
-	req.assert('username', 'Nama diperlukan').isAlpha().withMessage('Username harus terdiri dari angka dan huruf').notEmpty();
-    req.assert('email', 'E-mail tidak valid').notEmpty().withMessage('E-mail diperlukan').isEmail();
-    req.assert('first_name', 'Nama depan harus terdiri dari angka dan huruf').isAlpha();
-    req.assert('last_name', 'Nama belakang harus terdiri dari angka dan huruf').isAlpha();
+    req.assert('username', 'Username harus diisi').notEmpty();
+    req.assert('first_name', 'Nama depan harus diisi').notEmpty();
+    req.assert('last_name', 'Nama belakang harus diisi').notEmpty();
+    req.assert('email', 'E-mail harus diisi').notEmpty().withMessage('E-mail harus diisi').isEmail();
+    req.assert('alamat', 'Alamat harus diisi').notEmpty();
+    req.assert('jabatan', 'Jabatan harus diisi').notEmpty();
     //validasi
 	var errors = req.validationErrors();  
     console.log(errors);
 
     if (!errors)
     {
+
         v_username = req.sanitize( 'username' ).escape().trim();
-        v_email = req.sanitize( 'email' ).escape().trim();
         v_first_name = req.sanitize( 'first_name' ).escape().trim();
         v_last_name = req.sanitize( 'last_name' ).escape().trim();
+        v_email = req.sanitize( 'email' ).escape().trim();
+        v_alamat = req.sanitize( 'alamat' ).escape().trim();
+        v_jabatan = req.sanitize( 'jabatan' ).escape().trim();
 
-        models.Users.findOne({ where: { username: req.param('username') } }).then(regist => { 
+        models.Users.findOne({          
+            where: {
+                $or: [
+                {
+                    username: 
+                    {
+                        $eq: req.param('username') 
+                    }
+                },
+                { 
+                    email: 
+                    {
+                        $eq: req.param('email') 
+                    }
+                }
+                ]
+            } 
+            }).then(regist => { 
             if (regist == null) { 				
                		models.Users.create({ 
-						username: req.param('username'),
-						first_name: req.param('first_name'),
-						last_name: req.param('last_name'),
-						password: passwordEncripsi,
-						email: req.param('email'),
-						alamat: req.param('alamat'),
-						jabatan: req.param('jabatan'),
+                            username: v_username,
+                            first_name: v_first_name,
+                            last_name: v_last_name,
+                            password: passwordEncripsi,
+                            email: v_email,
+                            alamat: v_alamat,
+                            jabatan: v_jabatan
 					}).then(function (registerr) {
 						console.log(registerr);	
 						session_store.id = registerr.dataValues.id;
@@ -56,10 +78,12 @@ exports.store = function(req, res) {
             else{
 	                req.flash('msg_error', 'Maaf, username sudah digunakan...');
 	                res.render('register', {
-			            username: req.param('username'),
-			            email: req.param('email'),
-			            first_name: req.param('first_name'),
-			            last_name: req.param('last_name'),
+                        username: req.param('username'),
+                        email: req.param('email'),
+                        first_name: req.param('first_name'),
+                        last_name: req.param('last_name'),
+                        alamat: req.param('alamat'),
+                        jabatan: req.param('jabatan')
 			        });
             }
         });
@@ -80,6 +104,8 @@ exports.store = function(req, res) {
             email: req.param('email'),
             first_name: req.param('first_name'),
             last_name: req.param('last_name'),
+            alamat: req.param('alamat'),
+            jabatan: req.param('jabatan')
         });
     }
 
