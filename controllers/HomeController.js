cae2941e2364bf600b31028a72fbd3f960af2a6c
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-04-25 16:24:48 +0700

    Ubah halaman dashboard

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -3,5 +3,11 @@ var session_store;
 
 exports.index = function(req, res) {
 	session_store = req.session;
-	res.render('home/index',{ session_store:session_store }); 
+	models.Users.count().then(countuser => {
+		models.BahasaPemrogramans.count().then(bp => {
+			models.Artikels.count().then(artikels => {
+		  		res.render('home/index',{ session_store:session_store,countuser:countuser,countbp:bp,countartikel:artikels }); 
+			})
+		})
+	})
 };
